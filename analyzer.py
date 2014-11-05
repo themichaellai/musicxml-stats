525578b2e068fd804998ae0f8273e6fcf6d3e310
Michael Lai <themichaellai@gmail.com>
2014-11-04 21:04:27 -0500

    Parsing of key/time sig, measures

---
+++ analyzer.py
@@ -0,0 +1,73 @@
+import sys
+from bs4 import BeautifulSoup
+
+# Gets text inside xml node
+def extract_text(tag, child_name, css_selector=True):
+    if css_selector:
+        child = tag.select(child_name)
+    else:
+        child = tag(child_name)
+    return ''.join(t.get_text() for t in child)
+
+
+# Extracts text from adjacent nodes given the adjacent node names
+def extract_adjacent_nodes(parent_node, *node_names):
+    return [extract_text(parent_node, k, False) for k in node_names]
+
+
+def pitch_to_string(pitch_tag):
+    pitch_type = extract_text(pitch_tag, 'type')
+    step = extract_text(pitch_tag, 'pitch step')
+    return '%s (%s)' % (step, pitch_type)
+
+
+def rest_to_string(rest_tag):
+    t = rest_tag.select('type')
+    rest_type = extract_text(rest_tag, 'type')
+    return '%s rest' % (rest_type)
+
+
+def note_to_string(note_tag):
+    if note_tag('rest'):
+        return rest_to_string(note_tag)
+    elif note_tag('pitch'):
+        return pitch_to_string(note_tag)
+    else:
+        return 'ERROR'
+
+
+def key_to_string(key_tag):
+    fifths, major_minor = extract_adjacent_nodes(key_tag, 'fifths', 'mode')
+    return fifths, major_minor
+
+
+def time_to_string(time_tag):
+    lower, upper = extract_adjacent_nodes(time_tag, 'beat-type', 'beats')
+    return lower, upper
+
+
+def extract_measure_attributes(measure_tag):
+    attributes = measure_tag('attributes')
+    if attributes:
+        attributes = attributes[0]
+        key = attributes('key')
+        fifths, major_minor = key_to_string(key[0]) if key else (None, None)
+        time = attributes('time')
+        lower_time, upper_time = time_to_string(time[0]) if time else (None, None)
+        return fifths, major_minor, lower_time, upper_time
+
+
+
+if __name__ == '__main__':
+    with open(sys.argv[1], 'r') as f:
+        soup = BeautifulSoup(f.read())
+    parts = [p for s in soup('score-partwise') for p in s('part')]
+    measures = [m for p in parts for m in p('measure')]
+    print 'number of parts: %d' % len(parts)
+    print 'number of measures: %d' % len(measures)
+    notes = [m.select('note') for m in measures]
+    print 'number of notes in first measure %d' % len(notes[0])
+    print len(notes[0])
+    print extract_measure_attributes(measures[0])
+    for n in notes[0]:
+        print note_to_string(n)
